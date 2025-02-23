8d5107855f35b0ecfe7791b13e5a839e7cdeba3a
nkgrnkgr <beefupme@gmail.com>
2025-02-23 22:47:00 +0900

    mswでMockする例を追加

--- src/app/_components/DataFetch/index.test.tsx
+++ src/app/_components/DataFetch/index.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
 import { describe, expect, test, vi } from "vitest";
 import { DataFetch } from ".";
 import { repositories } from "../../../lib/repositories";
+import { server } from "../../../mocks/server";
 import { MockedAppProvider } from "../MockedAppRrovider";
 
 const TEST_DATA = {
@@ -12,9 +14,25 @@ const TEST_DATA = {
 };
 
 describe("DataFetch", () => {
-  test("dataが表示されている", async () => {
+  test("vi.spyOnでMockする", async () => {
     // FetcherをSpyして通信をMockするパターン
     vi.spyOn(repositories, "fetchTodo").mockResolvedValue(TEST_DATA);
+
+    render(
+      <MockedAppProvider>
+        <DataFetch todoId={1} />
+      </MockedAppProvider>,
+    );
+    const data = await screen.findByTestId("data");
+    expect(data).toBeInTheDocument();
+  });
+
+  test("mswでMockする", async () => {
+    const handler = http.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+      () => HttpResponse.json(TEST_DATA),
+    );
+    server.use(handler);
 
     render(
       <MockedAppProvider>
