1b7b07de18c9b277d9100858b6ca6d1635057f59
nkgrnkgr <beefupme@gmail.com>
2025-02-23 22:39:33 +0900

    update

--- src/app/_components/DataFetch/index.test.tsx
+++ src/app/_components/DataFetch/index.test.tsx
@@ -4,22 +4,24 @@ import { DataFetch } from ".";
 import { repositories } from "../../../lib/repositories";
 import { MockedAppProvider } from "../MockedAppRrovider";
 
-describe("DataFetch", () => {
-  vi.spyOn(repositories, "fetchTodo").mockResolvedValue({
-    userId: 1,
-    id: 1,
-    title: "test",
-    completed: false,
-  });
-  render(
-    <MockedAppProvider>
-      <DataFetch todoId={1} />
-    </MockedAppProvider>,
-  );
+const TEST_DATA = {
+  userId: 1,
+  id: 1,
+  title: "test",
+  completed: false,
+};
 
+describe("DataFetch", () => {
   test("dataが表示されている", async () => {
+    // FetcherをSpyして通信をMockするパターン
+    vi.spyOn(repositories, "fetchTodo").mockResolvedValue(TEST_DATA);
+
+    render(
+      <MockedAppProvider>
+        <DataFetch todoId={1} />
+      </MockedAppProvider>,
+    );
     const data = await screen.findByTestId("data");
     expect(data).toBeInTheDocument();
-    console.log(data.textContent);
   });
 });
